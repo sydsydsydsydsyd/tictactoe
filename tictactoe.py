595621669c4a83bc97856e411b69b14048b02965
Skin <Skin@s.local>
2020-08-29 19:07:40 -0400

    first attempt

--- tictactoe.py
+++ tictactoe.py
@@ -23,20 +23,19 @@ def player(board):
     Returns player who has the next turn on a board.
     """
 
-    """
-    num_xs = 0
-    num_os = 0
+    num_Xs = 0
+    num_Os = 0
     for i in board:
         for j in i:
-            if j == x:
-                num_xs += 1
-            if j == y:
-                num_ys +=1
-    if num_ys == num_xs:
+            if j == X:
+                num_Xs += 1
+            if j == O:
+                num_Ys +=1
+    if num_Ys == num_Xs:
         return X
-    if num_ys < num_xs:
+    if num_Ys < num_Xs:
         return Y
-    """
+
     raise NotImplementedError
 
 
@@ -45,15 +44,12 @@ def actions(board):
     Returns set of all possible actions (i, j) available on the board.
     """
 
-    """
     actions = set()
     for i in board:
         for j in i:
             if j == EMPTY:
                 actions += (i, j)
     return actions
-
-    """
 
     raise NotImplementedError
 
@@ -63,11 +59,10 @@ def result(board, action):
     Returns the board that results from making move (i, j) on the board.
     """
 
-    """
     board_copy = copy.deepcopy(board)
-    board_copy[i[j]] = action
+    board_copy[i][j] = action
     return board_copy
-    """
+
 
     raise NotImplementedError
 
@@ -77,29 +72,31 @@ def winner(board):
     Returns the winner of the game, if there is one.
     """
 
-    """
-    if board[0[0]] == board[0[1]] and board[0[1]] == board[0[2]]:
-        return board[0[0]]
-    elif board[1[0]] == board[1[1]] and board[1[1]] == board[1[2]]:
-        return board[0[0]]
-    elif board[2[0]] == board[2[1]] and board[2[1]] == board[2[2]]:
+    # horizontal
+    if board[0][0] == board[0][1] and board[0][1] == board[0][2]:
+        return board[0][0]
+    elif board[1][0] == board[1][1] and board[1][1] == board[1][2]:
+        return board[0][0]
+    elif board[2][0] == board[2][1] and board[2][1] == board[2][2]:
         return board [0[0]]
 
-    elif board[0[0]] == board[1[0]] and board[1[0]] == board[2[0]]:
+    # vertical
+    elif board[0][0] == board[1][0] and board[1][0] == board[2][0]:
         return board [0[0]]
-    elif board[0[1]] == board[1[1]] and board[1[1]] == board[2[1]]:
-        return board[0[0]]
-    elif board[0[2]] == board[1[2]] and board[1[2]] == board[2[2]]:
-        return board[0[0]]
+    elif board[0][1] == board[1][1] and board[1][1] == board[2][1]:
+        return board[0][0]
+    elif board[0][2] == board[1][2] and board[1][2] == board[2][2]:
+        return board[0][0]
 
-    elif board[0[0]] == board[1[1]] and board[1[1]] == board[2[2]]:
-        return board[0[0]]
-    elif board[2[0]] == board[1[1]] and board[1[1]] == board[0[2]]:
-        return board[0[0]]
+    # diagonal
+    elif board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+        return board[0][0]
+    elif board[2][0] == board[1][1] and board[1][1] == board[0][2]:
+        return board[0][0]
 
     else:
         return None
-    """
+
 
     raise NotImplementedError
 
@@ -109,12 +106,10 @@ def terminal(board):
     Returns True if game is over, False otherwise.
     """
 
-    """
-    if winner(board) or not actions(board):
+    if winner(board) or len(actions(board)) == 0:
         return True
     else:
         return False
-    """
 
     raise NotImplementedError
 
@@ -124,13 +119,11 @@ def utility(board):
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
 
-    """
     if winner(board) == X:
         return 1
     elif winner(board) == Y:
         return -1
     else: return 0
-    """
 
     raise NotImplementedError
 
@@ -140,9 +133,8 @@ def minimax(board):
     Returns the optimal action for the current player on the board.
     """
 
-    """
     score_list = []
-    turn = player(board)
+    # turn = player(board)
     actions = actions(board)
     for action in actions:
         score = 0
@@ -155,9 +147,8 @@ def minimax(board):
     for score in score_list:
         if score < min_score:
             min_score = score
-    actions[index of min_score] = optimal_action
+    actions[score_list.index(min_score)] = optimal_action
     return optimal_action
-    """
 
 
 
